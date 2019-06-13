9a425ccbaad32aa04d65553bac311c290b412bc4
Malay <44743128+MalaySaxena@users.noreply.github.com>
2019-06-13 22:50:16 +0530

    Add files via upload

---
+++ javassignment/src/main/classes/Customer.java
@@ -0,0 +1,21 @@
+package org.project.customer;
+
+public class Customer {
+ int id;
+ public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+String Name = new String();
+public String getName() {
+	return Name;
+}
+public void setName(String name) {
+	this.Name = name;
+}
+ 
+ 
+}
+ 
